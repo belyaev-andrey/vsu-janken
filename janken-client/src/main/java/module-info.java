f0132fa2265f728a146a76a3660c816838ce2f23
belyaev-andrey <belyaev.andrey@gmail.com>
2022-12-04 15:50:24 +0100

    Dependencies cleanup

--- janken-client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ru.vsu.csf.janken.client.jankenclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires ru.vsu.csf.janken.sdk.jankensdk;
-
-
-    opens ru.vsu.csf.janken.client to javafx.fxml;
-    exports ru.vsu.csf.janken.client;
-}
